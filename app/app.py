b552fc601e5c5d915f4103fda0565c3ec166471d
Meghana <meghana.shastri24@gmail.com>
2025-03-18 23:12:07 +0530

    update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
 
-app = FastAPI()
+app = FastAPI(openapi_url="/openapi.json")
 
 class PredictionRequest(BaseModel):
     features: list
@@ -12,13 +12,22 @@ class PredictionRequest(BaseModel):
 def load_model():
     global model
     try:
-        model = joblib.load("app/saved_models/random_forest_best_model.joblib")
+        model = joblib.load("saved_models/random_forest_best_model.joblib")
     except FileNotFoundError:
         raise RuntimeError("Model file not found. Train the model first.")
+
+@app.get("/home", summary="Home Endpoint", description="Welcome message for the API")
+def read_home():
+    return {"message": "Welcome to the prediction API!"}
 
 @app.post("/predict")
 def predict(request: PredictionRequest):
     features = pd.DataFrame([request.features])
     prediction = model.predict(features)[0]
+    if prediction == 0:
+        prediction = "Legitimate Transaction"
+    else:
+        prediction = "Fraudulent Transaction"
     probability = model.predict_proba(features)[0][1]
-    return {"prediction": prediction, "probability": probability}
+    return {"prediction": prediction, "confidence": probability}
+    
